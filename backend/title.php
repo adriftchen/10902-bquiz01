a862169521f00c2d5d9ebbcdd908d68f6c393324
mackliu <macklun@gmail.com>
2020-12-07 15:46:28 +0800

    標題圖片的文字增改刪查功能完成

--- backend/title.php
+++ backend/title.php
@@ -1,7 +1,7 @@
 
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli"><?=$tstr[$do];?></p>
-    <form method="post" action="./api/update.php?do=<?=$do;?>">
+    <form method="post" action="./api/edit.php">
         <table width="100%">
             <tbody>
                 <tr class="yel">
@@ -11,11 +11,28 @@
                     <td width="7%">刪除</td>
                     <td></td>
                 </tr>
+                <?php
+                $rows=$Title->all();
+
+                foreach($rows as $row){
+                ?>
+                <tr>
+                    <td width="45%"><img src="./img/<?=$row['img'];?>" style="width:300px;height:30px"></td>
+                    <td width="23%"><input type="text" name="text[]" value="<?=$row['text'];?>"></td>
+                    <td width="7%"><input type="radio" name="sh" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>></td>
+                    <td width="7%"><input type="checkbox" name="del[]" value="<?=$row['id'];?>"></td>
+                    <td><input type="button" value="更新圖片"></td>
+                    <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                </tr>
+                <?php
+                    }
+                ?>
             </tbody>
         </table>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
+                    <input type="hidden" name="table" value="<?=$do;?>">
                     <td width="200px"><input type="button"
                             onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/<?=$do;?>.php?table=<?=$do;?>&#39;)"
                             value="<?=$addstr[$do];?>"></td>
